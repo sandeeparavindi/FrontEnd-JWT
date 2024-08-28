8878fbfb63068a0a64141378208d7a1115f97b02
sandeeparavindi <sandeeparavindi1022@gmail.com>
2024-08-28 12:20:35 +0530

    add a headers

--- Spring-JWT-FrontEnd/js/dashBoard.js
+++ Spring-JWT-FrontEnd/js/dashBoard.js
@@ -1,7 +1,20 @@
 $(document).ready(function () {
     let token = localStorage.getItem("token");
     if (token){
-
+        $.ajax({
+            url:"http://localhost:8080/api/v1/blog/newMethod",
+            method:"GET",
+            contentType:"application/json",
+            headers:{
+                "Authorization":"Bearer " + token
+            },
+            success: function (response) {
+                console.log(response)
+            },
+            error: function (error) {
+                console.log(error)
+            }
+        })
     } else {
         window.location.href = "signIn.html"
     }
@@ -10,5 +23,6 @@ $(document).ready(function () {
 
 function logOut() {
     localStorage.removeItem("token")
+    window.location.href = "signIn.html"
 }
 
